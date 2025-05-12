30358924b1665ea6347813bfd251a7b7de542dbf
Akash Jayaraj <akash.jayaraj@bounteous.com>
2025-05-12 19:46:11 +0530

    First commit

---
+++ src/main/java/com/ajayaraj/lodgesage/LodgesageApplication.java
@@ -0,0 +1,13 @@
+package com.ajayaraj.lodgesage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LodgesageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LodgesageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ajayaraj/lodgesage/LodgesageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajayaraj.lodgesage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LodgesageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
