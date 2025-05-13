1d5c1aa634b80a92091e24c797782c4bfb0e5b94
AkashJayaraj <akashjayarajvkp2k02@gmail.com>
2025-05-14 01:13:12 +0530

    Added couple of methods in service controllers. Added swagger dependency. Can access in swagger-ui.

--- src/main/java/com/ajayaraj/lodgesage/controllers/LodgeController.java
+++ src/main/java/com/ajayaraj/lodgesage/controllers/LodgeController.java
@@ -1,9 +1,40 @@
 package com.ajayaraj.lodgesage.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ajayaraj.lodgesage.models.Lodge;
+import com.ajayaraj.lodgesage.services.LodgeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/lodges")
 public class LodgeController {
+
+    @Autowired
+    LodgeService lodgeService;
+
+    @GetMapping
+    public ResponseEntity<List<Lodge>> getAllLodges() {
+        List<Lodge> lodges = lodgeService.getAllLodges();
+        return ResponseEntity.ok(lodges);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Lodge> getLodgeById(@PathVariable Long id) {
+        Lodge lodge = lodgeService.getLodgeById(id);
+        if (lodge != null) {
+            return ResponseEntity.ok(lodge);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Lodge> addLodge(@RequestBody Lodge lodge) {
+        Lodge createdLodge = lodgeService.addLodge(lodge);
+        return ResponseEntity.status(201).body(createdLodge);
+    }
+
 }
--- src/main/java/com/ajayaraj/lodgesage/models/Lodge.java
+++ src/main/java/com/ajayaraj/lodgesage/models/Lodge.java
@@ -8,7 +8,6 @@ import jakarta.persistence.Id;
 import lombok.Data;
 
 @Entity
-@Data
 public class Lodge {
 
     @Id
@@ -17,4 +16,36 @@ public class Lodge {
     private String name;
     private String location;
     private int capacity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
 }
