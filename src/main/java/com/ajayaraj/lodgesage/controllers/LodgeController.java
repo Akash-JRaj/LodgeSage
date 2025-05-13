cf699ee9062426e9957de8380e1d241049247d98
AkashJayaraj <akashjayarajvkp2k02@gmail.com>
2025-05-14 01:19:28 +0530

    Added couple of api calls.

--- src/main/java/com/ajayaraj/lodgesage/controllers/LodgeController.java
+++ src/main/java/com/ajayaraj/lodgesage/controllers/LodgeController.java
@@ -37,4 +37,24 @@ public class LodgeController {
         return ResponseEntity.status(201).body(createdLodge);
     }
 
+    @PutMapping
+    public ResponseEntity<Lodge> updateLodge(@RequestBody Lodge lodge) {
+        Lodge updatedLodge = lodgeService.updateLodge(lodge);
+        if (updatedLodge != null) {
+            return ResponseEntity.ok(updatedLodge);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Lodge> deleteLodge(@PathVariable Long id) {
+        Lodge deletedLodge = lodgeService.deleteLodge(id);
+        if (deletedLodge != null) {
+            return ResponseEntity.ok(deletedLodge);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 }
