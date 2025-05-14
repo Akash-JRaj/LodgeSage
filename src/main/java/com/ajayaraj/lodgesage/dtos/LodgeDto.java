ce363d286f26a328845f49a76450108e03073e6d
AkashJayaraj <akashjayarajvkp2k02@gmail.com>
2025-05-14 19:08:28 +0530

    Added LodgeDto

---
+++ src/main/java/com/ajayaraj/lodgesage/dtos/LodgeDto.java
@@ -0,0 +1,41 @@
+package com.ajayaraj.lodgesage.dtos;
+
+
+public class LodgeDto {
+    private Long id;
+    private String name;
+    private String location;
+    private int capacity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+}
